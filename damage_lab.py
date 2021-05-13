90da2d7ebb0bd026c5e87fc571b8cd9e9c3efd1f
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2021-05-13 12:33:31 +0200

    Update damage_lab.py

--- damage_lab.py
+++ damage_lab.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 from numpy import floor
 
 def dmg(pwr, attack, defense, mod=1, lv=100):
-	#base=((((2*lv/5)+2)*pwr*attack/defense/50)+2)*mod
-	a = floor(2*lv/5)
-	ad = floor(attack/defense)
-	base = floor((floor(floor(floor((a+2)*pwr)*ad)/50)+2)*mod)
-	return(floor(np.array((base*0.85,base))))
+	a = floor(2 * lv / 5 + 2)
+	base = (floor(floor(a*pwr*attack/defense)/50)+2)
+	rolls = floor(np.array((base*0.85,base)))
+	return(floor(mod*rolls))
 
 dmg(100,atkstat,defstat,1.5)
 
@@ -33,38 +32,58 @@ dmg(100,atkstat,defstat)[1]/hpstat
 
 #--------- adamant krookodile earthquake vs slaking -------
 atkstat = stat(117, ev=252, nature='+')
-defstat = stat(100, ev=236, nature='+')
 hpstat = stat_hp(150,ev=16)
+defstat = stat(100, ev=236, nature='+')
 dmg(100,atkstat,defstat,1.5)/hpstat*100
 #----------------------------------------------------------
 
 
+
+#--------- modest espeon psychic vs bold blissey -------
+atkstat = stat(130, ev=252, nature='+')
+hpstat = stat_hp(255,ev=252)
+defstat = stat(135, ev=4, nature=1)
+dmg(90,atkstat,defstat,1.5)/hpstat*100
+#----------------------------------------------------------
+
+
 #================================================
-basehp=150
-basedef=100
+basehp=20
+basedef=230
 total_evs=252
-atkstat = stat(117,ev=252, nature='+')
+basepower=90
+atkstat = stat(130,ev=252, nature='+')
+modifier=1.5
 
 dpercents=np.zeros((253,2))
+dmgpoints  =np.zeros((253,2))
 ev_range = np.array(range(253))
 for def_evs in ev_range:
 	hpstat = stat_hp(basehp,ev=total_evs-def_evs)
 	defstat = stat(basedef,ev=def_evs, nature='+')
-	d = dmg(100,atkstat,defstat)*1.5
+	d = dmg(basepower,atkstat,defstat,modifier)
 	dpercents[def_evs,:] = d/hpstat*100
+	dmgpoints[def_evs,:] = d
 	#print(def_evs,total_evs-def_evs, d[1]/hpstat)
 
 
 fig = plt.figure(); ax = fig.add_subplot(111)
-ax.plot(ev_range, dpercents[:,0], label = 'Lo')
-ax.plot(ev_range, dpercents[:,1], label = 'Hi')
+ax.plot(ev_range, dmgpoints[:,0], label = 'Lo')
+ax.plot(ev_range, dmgpoints[:,1], label = 'Hi')
 ax.set_title(str(basehp) + ' base HP, ' + str(basedef) + ' base Def')
 ax.set_xlabel('EVs for defence', labelpad=10)
 ax.set_ylabel('% damage', labelpad=10)
-
 ax.legend(loc='lower left')
+
+print(np.argmin(dmgpoints,axis=0))
+
+
 plt.show()
 #================================================
+
+
+
+
 
 #================================================
 from scipy.optimize import minimize, minimize_scalar
