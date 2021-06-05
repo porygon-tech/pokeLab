348efd642e196fa5ad6ee049ac8f67fb5fc3e367
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2021-06-05 05:29:56 +0200

    Add files via upload

--- damage_lab.py
+++ damage_lab.py
@@ -2,13 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import floor
 
+def samplecolors(n, type='hex',palette=plt.cm.plasma):
+	if type == 'hex':
+		return list(map(plt.cm.colors.rgb2hex, list(map(palette, np.linspace(1,0,n)))))
+	elif type == 'rgba':
+		return list(map(palette, np.linspace(1,0,n)))
+
 def dmg(pwr, attack, defense, mod=1, lv=100):
 	a = floor(2 * lv / 5 + 2)
 	base = (floor(floor(a*pwr*attack/defense)/50)+2)
 	rolls = floor(np.array((base*0.85,base)))
 	return(floor(mod*rolls))
 
+def fulldmg(pwr, attack, defense, mod=1, lv=100):
+	a = floor(2 * lv / 5 + 2)
+	base = (floor(floor(a*pwr*attack/defense)/50)+2)
+	rands = np.array(range(85,101))/100
+	rolls = floor(base*rands)
+	return(floor(mod*rolls))
+
+
 dmg(100,atkstat,defstat,1.5)
+
 
 def stat(base,ev=0,iv=31,lv=100, nature=1):
 	if nature=='+':
@@ -22,6 +37,203 @@ def stat_hp(base,ev=0,iv=31,lv=100):
 	return(floor((2*base+iv+floor(ev/4))*lv/100)+lv+10)
 
 
+
+def plotseries(l, l_names=None):
+	fig = plt.figure(); ax = fig.add_subplot(111)
+	ax.plot(range(len(l)), l)
+	if l_names:
+		ax.set_xticklabels(l_names)
+	plt.show()
+
+
+'''
+def analyze(
+	basehp=20,
+	basedef=230,
+	defnature='+',
+	total_evs=252,
+	basepower=90,
+	atkstat = stat(130,ev=252, nature='+'),
+	modifier=1.5):
+	dpercents=np.zeros((253,2))
+	hp_dmg   =np.zeros((253,2))
+	ev_range = np.array(range(253))
+	for def_evs in ev_range:
+		hpstat = stat_hp(basehp,ev=total_evs-def_evs)
+		defstat = stat(basedef,ev=def_evs, nature=defnature)
+		d = dmg(basepower,atkstat,defstat,modifier)
+		dpercents[def_evs,:] = d/hpstat*100
+		hp_dmg[def_evs,:] = d
+		#print(def_evs,total_evs-def_evs, d[1]/hpstat)
+	#optDef = np.argmin(dpercents,axis=0)
+	#optDef = np.argmin(dpercents[:,1])
+	optDef = np.argmin(dpercents.mean(axis=1))
+	optHP  = 252 - optDef
+	fig = plt.figure(); ax = fig.add_subplot(111)
+	ax.plot(ev_range, dpercents[:,0], label = 'Lo')
+	ax.plot(ev_range, dpercents[:,1], label = 'Hi')
+	ax.set_title(str(basehp) + ' base HP, ' + str(basedef) + ' base Def. Optimal: ' + str(optDef) + 'Def; ' + str(optHP) + 'HP.')
+	ax.set_xlabel('defense EVs', labelpad=10)
+	ax.set_ylabel('% damage', labelpad=10)
+	ax.legend(loc='lower left')
+	plt.show()
+'''
+
+
+
+
+def analyze_full(
+	basehp=20,
+	basedef=230,
+	defnature='+',
+	total_evs=252,
+	basepower=90,
+	atkstat = stat(130,ev=252, nature='+'),
+	modifier=1.5):
+	palette = samplecolors(16)
+	dpercents=np.zeros((253,16))
+	hp_dmg   =np.zeros((253,16))
+	ev_range = np.array(range(253))
+	for def_evs in ev_range:
+		hpstat = stat_hp(basehp,ev=total_evs-def_evs)
+		defstat = stat(basedef,ev=def_evs, nature=defnature)
+		d = fulldmg(basepower,atkstat,defstat,modifier)
+		dpercents[def_evs,:] = d/hpstat*100
+		hp_dmg[def_evs,:] = d
+		#print(def_evs,total_evs-def_evs, d[1]/hpstat)
+	optDef = np.argmin(dpercents.mean(axis=1))
+	optHP  = 252 - optDef
+	fig = plt.figure(); ax = fig.add_subplot(111)
+	for x in range(16):
+		ax.plot(ev_range, dpercents[:,x], label = str(x), color=palette[x])
+	ax.set_title(str(basehp) + ' base HP, ' + str(basedef) + ' base Def. Optimal: ' + str(optDef) + 'Def; ' + str(optHP) + 'HP.')
+	ax.set_xlabel('defense EVs', labelpad=10)
+	ax.set_ylabel('% damage', labelpad=10)
+	ax.legend(loc='lower left')
+	plt.show()
+	return(dpercents)
+
+
+def showdata(mat, color=plt.cm.plasma, norm = False, symmetry=False):
+	mat = np.copy(mat)
+	if symmetry:
+		top = np.max([np.abs(np.nanmax(mat)),np.abs(np.nanmin(mat))])
+		plt.imshow(mat.astype('float32'), interpolation='none', cmap='seismic',vmax=top,vmin=-top)
+	elif norm:
+		plt.imshow(mat.astype('float32'), interpolation='none', cmap=color,vmax=1,vmin=0)
+	else:
+		plt.imshow(mat.astype('float32'), interpolation='none', cmap=color)
+	plt.colorbar()
+	plt.show()
+
+
+
+def damagematrix(
+	basehp=20,
+	basedef=230,
+	defnature='+',
+	total_evs=504,
+	basepower=90,
+	atkstat = stat(130,ev=252, nature='+'),
+	modifier=1.5):
+	M = np.zeros((253,253))
+	for j in range(253):
+		for i in range(253):
+			if (i+j)>total_evs:
+				M[i,j]=np.NaN
+			else:
+				hpstat = stat_hp(basehp,ev=i)
+				defstat = stat(basedef,ev=j, nature=defnature)
+				d = np.array(fulldmg(basepower,atkstat,defstat,modifier)).mean()
+				M[i,j] = d/hpstat
+	evspread = np.unravel_index(np.nanargmin(M), M.shape)
+	return(M,evspread)
+
+
+	
+
+
+#======================= krookodile earthquake vs shuckle =========================
+dperc=analyze_full(
+	basehp=20,
+	basedef=230,
+	defnature=1,
+	total_evs=252,
+	basepower=100,
+	atkstat = stat(117,ev=252, nature=1),
+	modifier=1.5)
+
+
+avgs = dperc.mean(axis=1)
+plotseries(avgs)
+
+
+atkstat = stat(117, ev=252)
+hpstat = stat_hp(20,ev=248)
+defstat = stat(230, ev=8)
+dmg(100,atkstat,defstat,1.5)/hpstat*100
+fulldmg(100,atkstat,defstat,1.5)
+
+#==================================================================================
+#================================ DAMAGE MATRIX ===================================
+#======================= nihilego power gem vs Porygon2 =========================
+M, evspread = damagematrix(
+	basehp=85,
+	basedef=90,
+	defnature=1,
+	total_evs=252,
+	basepower=100,
+	atkstat = stat(127,ev=252),
+	modifier=1.5)
+
+print('optimal: ' + str(evspread[0]) + 'HP, ' + str(evspread[1]) + 'Def')
+
+
+M = damagematrix(
+	basehp=85,
+	basedef=90,
+	defnature=1,
+	total_evs=328,
+	basepower=100,
+	atkstat = stat(127,ev=252),
+	modifier=1.5)
+
+
+#======================= nihilego power gem vs Porygon2 =========================
+analyze_full(
+	basehp=85,
+	basedef=90,
+	defnature=1,
+	total_evs=252,
+	basepower=100,
+	atkstat = stat(127,ev=252),
+	modifier=1.5)
+
+atkstat = stat(117, ev=252)
+hpstat = stat_hp(20,ev=248)
+defstat = stat(230, ev=8)
+dmg(100,atkstat,defstat,1.5)/hpstat*100
+#=======================================================================
+
+#nihilego vs wobbuffet
+M, evspread = damagematrix(
+	basehp=190,
+	basedef=58,
+	defnature='+',
+	total_evs=252,
+	basepower=90,
+	atkstat = stat(110,ev=252),
+	modifier=1.5)
+evspread
+
+showdata(M)
+
+
+
+
+
+
+
 #------------------------------------
 atkstat = stat(100)
 defstat = stat(100)
@@ -30,11 +242,13 @@ dmg(100,atkstat,defstat)[1]/hpstat
 #------------------------------------
 
 
+
 #--------- adamant krookodile earthquake vs slaking -------
 atkstat = stat(117, ev=252, nature='+')
 hpstat = stat_hp(150,ev=16)
 defstat = stat(100, ev=236, nature='+')
 dmg(100,atkstat,defstat,1.5)/hpstat*100
+fulldmg(100,atkstat,defstat,1.5)
 #----------------------------------------------------------
 
 
@@ -50,36 +264,62 @@ dmg(90,atkstat,defstat,1.5)/hpstat*100
 #================================================
 basehp=20
 basedef=230
+defnature='+'
 total_evs=252
 basepower=90
 atkstat = stat(130,ev=252, nature='+')
 modifier=1.5
 
-dpercents=np.zeros((253,2))
-dmgpoints  =np.zeros((253,2))
+dpercents=np.zeros((253,16))
+hp_dmg   =np.zeros((253,16))
 ev_range = np.array(range(253))
 for def_evs in ev_range:
 	hpstat = stat_hp(basehp,ev=total_evs-def_evs)
-	defstat = stat(basedef,ev=def_evs, nature='+')
-	d = dmg(basepower,atkstat,defstat,modifier)
+	defstat = stat(basedef,ev=def_evs, nature=defnature)
+	#d = dmg(basepower,atkstat,defstat,modifier)
+	d = fulldmg(basepower,atkstat,defstat,modifier)
 	dpercents[def_evs,:] = d/hpstat*100
-	dmgpoints[def_evs,:] = d
+	hp_dmg[def_evs,:] = d
 	#print(def_evs,total_evs-def_evs, d[1]/hpstat)
 
 
 fig = plt.figure(); ax = fig.add_subplot(111)
-ax.plot(ev_range, dmgpoints[:,0], label = 'Lo')
-ax.plot(ev_range, dmgpoints[:,1], label = 'Hi')
+ax.plot(ev_range, dpercents[:,0], label = 'Lo')
+ax.plot(ev_range, dpercents[:,1], label = 'Hi')
 ax.set_title(str(basehp) + ' base HP, ' + str(basedef) + ' base Def')
 ax.set_xlabel('EVs for defence', labelpad=10)
 ax.set_ylabel('% damage', labelpad=10)
 ax.legend(loc='lower left')
 
-print(np.argmin(dmgpoints,axis=0))
+print(np.argmin(dpercents,axis=0))
 
 
 plt.show()
 #================================================
+
+
+
+
+
+
+
+
+
+
+
+
+analyze(basehp=80,basedef=100,defnature='+',total_evs=252,basepower=90,atkstat = stat(130,ev=252, nature='+'),modifier=1.5)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- typeEff.py
+++ typeEff.py
@@ -1,5 +1,8 @@
 import numpy as np
 #		NOR	FGH	FLY	POI	GRN	ROC	BUG	GHO	STL	FIR	WAT	GRA	ELC	PSY	ICE	DRA	DAK	FAY
+typenames = ('NORMAL','FIGHTING','FLYING','POISON','GROUND','ROCK','BUG','GHOST','STEEL','FIRE','WATER','GRASS','ELECTRIC','PSYCHIC','ICE','DRAGON','DARK','FAIRY')
+n = len(typenames)
+
 NORMAL = [1,1,1,1,1,0.5,1,0,0.5,1,1,1,1,1,1,1,1,1]
 FIGHTING = [2,1,1,0.5,0.5,2,0.5,0,2,1,1,1,1,0.5,2,1,2,0.5]
 FLYING = [1,2,1,1,1,0.5,2,1,0.5,1,1,2,0.5,1,1,1,1,1]
@@ -21,14 +24,31 @@ FAIRY = [1,2,1,0.5,1,1,1,1,0.5,0.5,1,1,1,1,1,2,2,1]
 
 effectiveness = np.matrix([NORMAL,FIGHTING,FLYING,POISON,GROUND,ROCK,BUG,GHOST,STEEL,FIRE,WATER,GRASS,ELECTRIC,PSYCHIC,ICE,DRAGON,DARK,FAIRY])
 
-typ = {'NORMAL': 0,'FIGHTING': 1,'FLYING': 2,'POISON': 3,'GROUND': 4,'ROCK': 5,'BUG': 6,'GHOST': 7,'STEEL': 8,'FIRE': 9,'WATER': 10,'GRASS': 11,'ELECTRIC': 12,'PSYCHIC': 13,'ICE': 14,'DRAGON': 15,'DARK': 16,'FAIRY': 17}
+typ = {'NORMAL': 0,
+       'FIGHTING': 1,
+       'FLYING': 2,
+       'POISON': 3,
+       'GROUND': 4,
+       'ROCK': 5,
+       'BUG': 6,
+       'GHOST': 7,
+       'STEEL': 8,
+       'FIRE': 9,
+       'WATER': 10,
+       'GRASS': 11,
+       'ELECTRIC': 12,
+       'PSYCHIC': 13,
+       'ICE': 14,
+       'DRAGON': 15,
+       'DARK': 16,
+       'FAIRY': 17}
 
 def eff(type1,type2):
 	return(effectiveness[typ[type1],typ[type2]])
 
 print(typ['FIGHTING'])
 print(effectiveness.shape)
-print(eff('FIGHTING','DARK'))
+print(eff('FIGHTING','DARK')) #prints effectiveness from fighting against dark
 
 
 #		NOR	FGH	FLY	POI	GRN	ROC	BUG	GHO	STL	FIR	WAT	GRA	ELC	PSY	ICE	DRA	DAK	FAY
@@ -50,3 +70,58 @@ print(eff('FIGHTING','DARK'))
 #	DRA	1	1	1	1	1	1	1	1	0.5	1	1	1	1	1	1	2	1	0
 #	DAK	1	0.5	1	1	1	1	1	2	1	1	1	1	1	2	1	1	0.5	0.5
 #	FAY	1	2	1	0.5	1	1	1	1	0.5	0.5	1	1	1	1	1	2	2	1
+
+
+#////////////// 2-typed mons //////////////////////
+resist_sum = effectiveness.T * effectiveness
+
+best = np.unravel_index(resist_sum.argmin(), resist_sum.shape)
+typenames[best[0]], typenames[best[1]]
+
+ncomb=n**2
+with open('two_types.txt', 'w') as f:
+	for i in range(ncomb):
+		best = np.unravel_index(np.argsort(resist_sum,axis=None)[0,i], resist_sum.shape)
+		f.write(str(typenames[best[0]]) + ' ' + str(typenames[best[1]]) + ': ' + str(resist_sum[best]) + '\n')
+		#print(str(typenames[best[0]]) + ' ' + str(typenames[best[1]]) + ': ' + str(resist_sum[best]))
+#//////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+#////////////// 3-typed mons //////////////////////////////////////////
+resist_sum3 = np.zeros((n,n,n))
+n=len(typenames)
+for x in range(n):
+	for y in range(n):
+		for z in range(n):
+			resist_sum3[x,y,z] = np.sum(np.multiply(np.multiply(effectiveness[:,x],effectiveness[:,y]),effectiveness[:,z]))
+
+
+#best = np.unravel_index(resist_sum3.argmin(), resist_sum3.shape)
+#typenames[best[0]], typenames[best[1]], typenames[best[2]]
+
+#resist_sum3 = np.triu(resist_sum3)
+ncomb=n**3
+with open('three_types.txt', 'w') as f:
+	for i in range(ncomb):
+		best = tuple(np.array(np.unravel_index(np.argsort(resist_sum3,axis=None), resist_sum3.shape))[:,i])
+		f.write(str(typenames[best[0]]) + ' ' + str(typenames[best[1]]) + ' ' + str(typenames[best[2]]) + ': ' + str(resist_sum3[best]) + '\n')
+		#print(str(typenames[best[0]]) + ' ' + str(typenames[best[1]]) + ' ' + str(typenames[best[2]]) + ': ' + str(resist_sum3[best]))
+'''
+ncomb=n**3
+for i in range(ncomb):
+	best = tuple(np.array(np.unravel_index(np.argsort(resist_sum3,axis=None), resist_sum3.shape))[:,i])
+	if best[0] == typ['WATER'] and best[1] == typ['POISON']:
+		print(str(typenames[best[0]]) + ' ' + str(typenames[best[1]]) + ' ' + str(typenames[best[2]]) + ': ' + str(resist_sum3[best]))
+'''
+
+
+
+#//////////////////////////////////////////////////////////////////////
